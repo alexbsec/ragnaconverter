065d3982c7c48c2b2f26849cb988f304808385a6
alexbsec <j.a.c.buschinelli@gmail.com>
2025-05-09 19:56:57 -0300

    logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,3 +1,38 @@
 package logger
 
-import 
+import (
+	"log"
+	"os"
+)
+
+type ConsoleLogger struct {
+	debug *log.Logger
+	info  *log.Logger
+	warn  *log.Logger
+	err   *log.Logger
+}
+
+func NewConsoleLogger() *ConsoleLogger {
+	return &ConsoleLogger{
+		debug: log.New(os.Stdout, "[DEBUG]: ", log.LstdFlags),
+		info:  log.New(os.Stdout, "[INFO]: ", log.LstdFlags),
+		warn:  log.New(os.Stdout, "[WARN]: ", log.LstdFlags),
+		err:   log.New(os.Stderr, "[ERROR]: ", log.LstdFlags),
+	}
+}
+
+func (cl *ConsoleLogger) Debug(v ...any) {
+	cl.debug.Println(v...)
+}
+
+func (cl *ConsoleLogger) Info(v ...any) {
+	cl.info.Println(v...)
+}
+
+func (cl *ConsoleLogger) Warn(v ...any) {
+	cl.warn.Println(v...)
+}
+
+func (cl *ConsoleLogger) Error(v ...any) {
+	cl.err.Println(v...)
+}
